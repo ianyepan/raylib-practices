848d74d068d3c52ee5d6f2279d1ef4076d2f3f48
ianpan870102 <ianyepan@outlook.com>
2020-09-24 18:39:10 +0800

    Working on zombie texture!

--- src/texture.cpp
+++ src/texture.cpp
@@ -1,32 +1,35 @@
 #include "../../../raylib-cpp/include/raylib-cpp.hpp"
 #include "raylib.h"
 
-#define NUM_TEXTURES 7 // Currently we have 7 generation algorithms
 const raylib::Color BG_COLOR{3, 1, 146};
-const int screenWidth = 800;
-const int screenHeight = 450;
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 450;
 
-auto checkedImage = raylib::Image::GenChecked(screenWidth, screenHeight, 10, 10, BG_COLOR, BG_COLOR.Fade(0.9f));
+auto checkedImage = raylib::Image::GenChecked(SCREEN_WIDTH, SCREEN_HEIGHT, 10, 10, BG_COLOR, BG_COLOR.Fade(0.9f));
 raylib::Texture2D backgroundTexture;
 
 int main(void)
 {
-  InitWindow(screenWidth, screenHeight, "raylib [textures] example - procedural images generation");
+  raylib::Window window{SCREEN_WIDTH, SCREEN_HEIGHT, "Texture practice"};
 
-  SetTargetFPS(60);
+  ::SetTargetFPS(60);
 
   // Has to be defined inside main, WHY?
   backgroundTexture.LoadFromImage(checkedImage);
 
-  while (!WindowShouldClose())
+  raylib::Texture2D zombie{"../images/zombie.png"};
+
+  while (!window.ShouldClose())
   {
-    BeginDrawing();
+    ::BeginDrawing();
 
     ::ClearBackground(raylib::Color::RayWhite);
 
     ::DrawTexture(backgroundTexture, 0, 0, WHITE);
 
-    EndDrawing();
+    zombie.Draw(raylib::Vector2{0, 0}, raylib::Color::RayWhite);
+
+    ::EndDrawing();
   }
 
   return 0;
